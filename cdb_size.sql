572a8487b9e9d5c327b9de695e6ac2bfad8f0290
Valeh Agayev <valeh.agayev@gmail.com>
2017-02-22 22:25:48 +0400

    Create cdb_size.sql

---
+++ cdb_size.sql
@@ -0,0 +1,10 @@
+SELECT   (SELECT SUM (bytes) / 1024 / 1024 / 1024 data_size
+            FROM cdb_data_files)
+       + (SELECT NVL (SUM (bytes), 0) / 1024 / 1024 / 1024 temp_size
+            FROM cdb_temp_files)
+       + (SELECT SUM (bytes) / 1024 / 1024 / 1024 redo_size FROM sys.v_$log)
+       + (SELECT SUM (BLOCK_SIZE * FILE_SIZE_BLKS) / 1024 / 1024 / 1024
+                    controlfile_size
+            FROM v$controlfile)
+          Size_in_GB
+  FROM DUAL;
